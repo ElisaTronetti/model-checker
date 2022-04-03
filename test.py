919458cebf5453792254d3e991b6e16a2a9fcd00
ElisaTronetti <elisa.tronetti@studio.unibo.it>
2022-04-03 20:44:57 +0000

    Added formulas for the assignment in the test file

--- test.py
+++ test.py
@@ -2,7 +2,31 @@ from model_checker import modelcheck
 
 # Used for test and LTL formula on all the paths files
 
-form = "(employee_right || employee_left) U employee_trans"
+#LTL formulas for the assignment
+formula_1_a = "G(!(wolf_right && goat_right && !employee_right) && !(wolf_left && goat_left && !employee_left))"
+
+formula_1_b = "G(!(popeye_right && spinach_right && !employee_right) && !(popeye_left && spinach_left && !employee_left))"
+
+formula_1_c = """G(!(popeye_right && wine_right && computer_right && !employee_right) && 
+!(popeye_left && wine_left && computer_left && !employee_left))"""
+
+wolf_and_goat_unsupervised_right = "(wolf_right && goat_right && !employee_right)"
+wolf_and_goat_unsupervised_left = "(wolf_left && goat_left && !employee_left)"
+wolf_and_goat_unsupervised_right_for_four_states = "("+ wolf_and_goat_unsupervised_right +" && X("+ wolf_and_goat_unsupervised_right +" ) && X(X("+ wolf_and_goat_unsupervised_right +")) && X(X(X("+ wolf_and_goat_unsupervised_right +"))))"
+wolf_and_goat_unsupervised_left_for_four_states = "("+ wolf_and_goat_unsupervised_left +" && X("+ wolf_and_goat_unsupervised_left +" ) && X(X("+ wolf_and_goat_unsupervised_left +")) && X(X(X("+ wolf_and_goat_unsupervised_left +"))))"
+formula_2 = "G(!("+ wolf_and_goat_unsupervised_right_for_four_states +") && !("+ wolf_and_goat_unsupervised_left_for_four_states +"))"
+
+employee_right_for_four_states = "(employee_right && X(employee_right) && X(X(employee_right)) && X(X(X(employee_right))))"
+employee_left_for_four_states = "(employee_left && X(employee_left) && X(X(employee_left)) && X(X(X(employee_left))))"
+formula_3 = "G(!("+ employee_right_for_four_states +") && !("+ employee_left_for_four_states +"))"
+
+popeye_with_employee = "(popeye_right && employee_right && !spinach_right)"
+employee_departed = "(popeye_right && !employee_right)"
+formula_4 = "G(!("+ popeye_with_employee +" && X("+ popeye_with_employee +" U ("+ employee_departed +" && X("+ employee_departed +" U "+ popeye_with_employee +")))))"
+
+formula_5 = "G(goat_trans -> X(!goat_trans U sheep_trans))"
+
+formula_6 = "G((employee_right U employee_trans) || (employee_left U employee_trans))"
 
 path = [
 "paths/path0.txt",
@@ -16,4 +40,5 @@ path = [
 ]
 
 for p in path:
-    modelcheck(p,form)
+    modelcheck(p,formula_6)
+    
